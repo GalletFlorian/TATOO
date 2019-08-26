73e5c9312b83915000f485eeb2503248a23fb714
Florian Gallet <53513832+GalletFlorian@users.noreply.github.com>
2019-08-26 15:33:56 +0200

    New version of Function.py for Python 3

--- Function.py
+++ Function.py
@@ -2,24 +2,16 @@ import numpy as np
 from scipy.interpolate import griddata
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from math import log10
-from scipy import interpolate
-from scipy import interp
 import random
 import math
 import sys
-import os
-import os.path
 from scipy import stats
 from scipy.optimize import curve_fit
 
-#chi2 threshold
+#chi2 threshold used in Find_4 and in Is_empty
 chi2lim = 100.0
 
-
-
-
-		
+	
 arr_agemod_min = []
 arr_agemod_max = []
 
@@ -28,45 +20,43 @@ arr_agemod_min_rand = []
 arr_agemod_max_rand = []
 
 #Useful functions
-def floatt(number) :
+
+def floatt3(number) : 
+#return a float with 3 digit precision
     return "%.3f"%number
     
-def floatt2(number) :
+def floatt2(number) : 
+#return a float with 2 digit precision
     return "%.2f"%number
     
-def floatt3(number) :
+def floatt1(number) : 
+#return a float with 1 digit precision
 	return "%.1f"%number
 
-def func_sq(x,a,b,c):
+def func_sq(x,a,b,c): 
+#return a power law function value
 	return a*x**2.0+ b*x + c
 	
-def func_lin(x,a,b):
+def func_lin(x,a,b): 
+#return a linear function value
 	return a*x + b
 
-def linear(x,a,b):
-	return a*x + b
 	
-	
-def clean(array):
+def clean(array): 
+#function that "clean" a given array
 	array = [0.0,0.0,0.0,0.0]
 	return array
 	
-x = []
-y = []	
-def rsquared(x, y):
-	""" Return R^2 where x and y are array-like."""
-	slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-	return r_value**2
-
-
-def Is_empty(mstar,sma,prot):
-
+		
+def Is_empty(mstar,sma,prot): 
+#Function used to check the content of a given pre-compiled age exploration files
+#If empty return flag = 0 else flag > 0
 	arr_ai = []
 	arr_roti = []
 	arr_chi2 = []
 	arr_massp = []
 	arr_age = []
-	filename = "./%sMsol/Explo_100_%s_%s_sort.dat" %(mstar,floatt(sma),floatt2(prot))
+	filename = "./%sMsol/Explo_100_%s_%s_sort.dat" %(mstar,floatt3(sma),floatt2(prot))
 	flag = 0
 	inputfile = open(filename)
 	next(inputfile)
@@ -85,7 +75,9 @@ def Is_empty(mstar,sma,prot):
 	return flag
 	
 def Control_file(protin,smain,iprot,isma,mstar,smamin,protmin,smamax,protmax):
-
+#Function used to control on the containt of the pre-compiled exploration files using 
+#the function Is_empty above.
+#Re-ajust the 4 corner points if needed
 	
 	prot = protin 
 	sma = smain
@@ -94,10 +86,6 @@ def Control_file(protin,smain,iprot,isma,mstar,smamin,protmin,smamax,protmax):
 
 	size_prot = len(prot)
 	size_sma = len(sma)
-	
-	
-	#Control on the containt of the files
-	#Re-ajust the 4 corner points if needed
 	
 	#smamax
 	flag = Is_empty(mstar,smamax,protmin)
@@ -150,7 +138,7 @@ def Control_file(protin,smain,iprot,isma,mstar,smamin,protmin,smamax,protmax):
 		else:
 			protmin = prot[index_ref_prot]
 			flag = Is_empty(mstar,smamin,protmin)	
-			print sma[index_ref_sma-index]
+			print (sma[index_ref_sma-index])
 			smamin = sma[index_ref_sma-index]
 			index = index + 1		
 		
@@ -174,17 +162,41 @@ def Control_file(protin,smain,iprot,isma,mstar,smamin,protmin,smamax,protmax):
 	return smamin,protmin,smamax,protmax		
 		
 
-#Function used to get the age estimation, at the selected planetary mass, of the 4 couples that surround the observed couple.
-def Find_4(sma,prot,mp,mstar,i,sma_list,prot_list,age_list,massp_list,masss_list):
 
+def Find_4(sma,prot,mp,mstar,i,sma_list,prot_list,age_list,massp_list,masss_list):
+#Function used to get the age estimation, at the selected planetary mass, 
+#of the 4 couples that surround the observed couple.
+#Return the Pearson correlation coefficient of M_p vs. Age of the file "filename"
 	
 	filename = "./%sMsol/Explo_100_%s_%s_sort.dat" %(mstar,sma,prot)
+
 	arr_ai = []
 	arr_roti = []
 	arr_chi2 = []
 	arr_massp = []
 	arr_age = []
+	
+	arr_ai_all = []
+	arr_roti_all = []
+	arr_chi2_all = []
+	arr_massp_all = []
+	arr_age_all = []
+	arr_weight_all = []
+	
+	for k in range(0,7): 
+		arr_ai_all.append([])
+		arr_roti_all.append([])
+		arr_chi2_all.append([])
+		arr_massp_all.append([])
+		arr_age_all.append([])
+		arr_weight_all.append([])
+	
+	arr_mean_age_coef = []
+	arr_massp_coef = []
+	
 	count = 0
+	
+	count_all = []
 	inputfile = open(filename)
 	next(inputfile)
 	for line in inputfile:
@@ -195,44 +207,38 @@ def Find_4(sma,prot,mp,mstar,i,sma_list,prot_list,age_list,massp_list,masss_list
 			arr_chi2.append(float(chi2))
 			arr_age.append(float(age))
 			arr_massp.append(float(massp))
+				
+			k = int(2*float(massp)-1)
+					
+			arr_ai_all[k].append(float(ai))
+			arr_roti_all[k].append(float(roti))
+			arr_chi2_all[k].append(float(chi2))
+			arr_weight_all[k].append(1.0/float(chi2))
+			arr_age_all[k].append(float(age))
+			arr_massp_all[k].append(float(massp))
+					
 			count = count + 1
-	    		
+	    				    		 		
 	inputfile.close()	
 
-	if count > 0:
-		#popt, pcov = curve_fit(func_sq, np.array(arr_massp), np.array(arr_age),sigma=np.array(arr_chi2))
-		#a = popt[0]
-		#b = popt[1]
-		#c = popt[2]
-
-		popt, pcov = curve_fit(func_lin, np.array(arr_massp), np.array(arr_age),sigma=np.array(arr_chi2))
+	count_mp = 0
+	for k in range(0,7):
+	
+		if arr_weight_all[k]:
+			arr_mean_age_coef.append(np.average(np.array(arr_age_all[k]), axis=None, weights=np.array(arr_weight_all[k])))
+			arr_massp_coef.append(arr_massp_all[k][0])
+			
+			mean_age_tmp = np.average(np.array(arr_age_all[k]), axis=None, weights=np.array(arr_weight_all[k]))
+			
+			count_mp = count_mp + 1
+			
+	if count > 0 and count_mp > 1:
+		
+		popt, pcov = curve_fit(func_lin, np.array(arr_massp_coef), np.array(arr_mean_age_coef))
 		a = popt[0]
 		b = popt[1]
-		
-		#linear interpolation
 		time_mod = a*float(mp)+b
-		
-		#time_mod = a*float(mp)**2.0 + b*float(mp) + c
-		
-		
-		
-		#residuals = (np.array(arr_age) - func_lin(np.array(arr_massp),*popt)) / np.array(arr_chi2)
-		#RMSE = (sum(residuals**2)/(residuals.size-3))**0.5
-
-		#meanage = np.mean(np.array(arr_age))
-		#residu_age = np.array(arr_age) - meanage
-		#std_age = np.std(arr_age)
-		
-		#meanmp =  np.mean(func_lin(np.array(arr_massp),*popt))
-		#residu_mp = func_lin(np.array(arr_massp),*popt) - meanmp
-		#std_mp = np.std(func_lin(np.array(arr_massp),*popt))
-		
-		#if std_mp != 0.0 and std_age != 0.0:
-		#	coef =  np.mean(residu_age*residu_mp)/(std_mp*std_age)
-		#else:
-		#	coef = 0.0	
-
-			
+					
 		sma_list[i] = float(sma)
 		prot_list[i] = float(prot)
 		age_list[i] = float(time_mod)
@@ -245,35 +251,37 @@ def Find_4(sma,prot,mp,mstar,i,sma_list,prot_list,age_list,massp_list,masss_list
 		massp_list[i] = float(mp)
 		masss_list[i] = float(mstar)
 		popt = [0.0, 0.0]
-	
-	#print np.array(arr_age),func_lin(np.array(arr_massp),*popt)
-	coef  = stats.spearmanr(np.array(arr_age),func_lin(np.array(arr_massp),*popt))[0]	
-	#coef = 1
 		
+	coef_spear = stats.spearmanr(np.array(arr_mean_age_coef),func_lin(np.array(arr_massp_coef),*popt))[0]	
+	coef_pear  = stats.pearsonr(np.array(arr_mean_age_coef),func_lin(np.array(arr_massp_coef),*popt))[0]	
+	
+	coef = coef_pear
+	
 	if coef != coef:
-		#print coef,np.array(arr_age),func_lin(np.array(arr_massp),*popt)
 		coef = 0.0	
 	
 	return coef
 	
-#Given the information of Find_4, Find_age perform a 3D interpolation using the Griddata tool.
 def Find_age(sma,prot,sma_list,prot_list,age_list,massp_list,masss_list):
+#Given the information of Find_4, Find_age perform a 3D interpolation using the Griddata tool.
+#Return the estimated 3D age 
 
 	arr_xy = []
 
-
 	for i in range(0,4):
 		arr_xy.append([sma_list[i],prot_list[i]])
-
+	
+	age = 0.0
+	
 	if sma_list[0] > 0:
 		smaobs = float(sma)
 		protobs = float(prot)
-		grid = griddata(arr_xy, age_list, (smaobs,protobs), method='cubic')
+		age = griddata(arr_xy, age_list, (smaobs,protobs), method='cubic')
 	else:
-		grid = 0.0 
+		age = 0.0 
 	
-	if grid != grid:
-		#sys.exit("Problem with NaN in grid")
-		grid = -1.0
-		
-	return grid
+	if age != age:
+		age = -1.0
+
+	return age
+	
