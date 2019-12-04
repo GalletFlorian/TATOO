e4bfa273f9bc18e80ba7607a41273da7de40c153
GalletFlorian <florian.gallet@gmail.com>
2019-12-04 13:29:20 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+__version__ = "1.0"
+
+setup(name='TATOO_code',
+      version=__version__,
+      description='Inferring stellar ages using tidal-chronology',
+      url='https://github.com/GalletFlorian/TATOO',
+      author='Florian Gallet',
+      author_email='florian.gallet@gmail.com',
+      license='CNES/CNRS',
+      packages=['TATOO'],
+      install_requires=['numpy', 'scipy', 'matplotlib', 'random', 'math', 'stats', 'tkinter'],
+      zip_safe=False)
